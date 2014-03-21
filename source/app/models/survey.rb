6e503a34ab606a5bdf5afe9589b1d22ef35fd6c2
= <=>
2014-03-21 16:10:43 -0500

    Adds models and associations so you can associate with models

--- source/app/models/survey.rb
+++ source/app/models/survey.rb
@@ -1,3 +1,6 @@
 class Survey < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :creator, class_name: "User"
+  has_many :questions
+  has_many :taken_surveys
+  has_many :users, through: :taken_surveys
 end
--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_secure_password
+  has_many :surveys, foreign_key: "creator_id"
+  has_many :taken_surveys, foreign_key: "taker_id"
+  has_many :surveys, through: :taken_surveys
 end
