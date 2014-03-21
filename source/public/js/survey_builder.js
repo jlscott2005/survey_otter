b15aca7b6c83d245da9b3c5c1b4a31155caf48e5
& Tony Ta <pair+CalderWishne.tonyta@devbootcamp.com>
2014-03-21 18:34:37 -0500

    just added survey builder

--- source/public/js/survey_builder.js
+++ source/public/js/survey_builder.js
@@ -14,25 +14,3 @@ $(document).ready(function(){
     $(this).parent().remove();
   });
 });
-
-
-// //initialize with 3
-// var startingNo = 3;
-// var $node = "";
-// for(varCount=0;varCount<=startingNo;varCount++){
-//   var displayCount = varCount+1;
-//   $node += '<p><label for="var'+displayCount+'">Variable '+displayCount+': </label><input type="text" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar">Remove Variable</span></p>';
-// }
-// $('form').prepend($node);
-
-// $('form').on('click', '.removeVar', function(){
-//   $(this).parent().remove();
-//   //varCount--;
-// });
-
-// $('#addVar').on('click', function(){
-//   //new node
-//   varCount++;
-//   $node = '<p><label for="var'+varCount+'">Variable '+varCount+': </label><input type="text" name="var'+varCount+'" id="var'+varCount+'"><span class="removeVar">Remove Variable</span></p>';
-//   $(this).parent().before($node);
-// });
