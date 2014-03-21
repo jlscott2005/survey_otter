63775b43a9dbf38f97a156313bbe8d92d13a4c82
& Tony Ta <pair+CalderWishne.tonyta@devbootcamp.com>
2014-03-21 16:29:00 -0500

    adds view for landing page and scaffolding for survey builder

---
+++ source/public/js/survey_builder.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){});
+
+
+// //initialize with 3
+// var startingNo = 3;
+// var $node = "";
+// for(varCount=0;varCount<=startingNo;varCount++){
+//   var displayCount = varCount+1;
+//   $node += '<p><label for="var'+displayCount+'">Variable '+displayCount+': </label><input type="text" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar">Remove Variable</span></p>';
+// }
+// $('form').prepend($node);
+
+// $('form').on('click', '.removeVar', function(){
+//   $(this).parent().remove();
+//   //varCount--;
+// });
+
+// $('#addVar').on('click', function(){
+//   //new node
+//   varCount++;
+//   $node = '<p><label for="var'+varCount+'">Variable '+varCount+': </label><input type="text" name="var'+varCount+'" id="var'+varCount+'"><span class="removeVar">Remove Variable</span></p>';
+//   $(this).parent().before($node);
+// });
