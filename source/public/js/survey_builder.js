3e4e6a9eccc913664169ab67305d16a2188d2316
& Tony Ta <pair+CalderWishne.tonyta@devbootcamp.com>
2014-03-21 17:22:27 -0500

    added survey creator frontend

--- source/public/js/survey_builder.js
+++ source/public/js/survey_builder.js
@@ -1,4 +1,19 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+  $('form').on('click', '.add-question', function(event){
+    event.preventDefault();
+    console.log('hello');
+    $($(this).siblings('ol')).append("<li><input type='text' name='survey[questions][][question]' placeholder='question'><button class='remove-button'>✖</button><button class='button add-option'>add option</button><ul><li><input type='text' name='survey[questions][][options][]' placeholder='option'><button class='remove-button'>✖</button></li></ul></li>");
+  });
+  $('form').on('click', '.add-option', function(event){
+    event.preventDefault();
+    console.log('hello');
+    $($(this).siblings('ul')).append("<li><input type='text' name='survey[questions][][options][]' placeholder='option'><button class='remove-button'>✖</button></li>");
+  });
+  $('form').on('click', '.remove-button', function(event){
+    event.preventDefault();
+    $(this).parent().remove();
+  });
+});
 
 
 // //initialize with 3
