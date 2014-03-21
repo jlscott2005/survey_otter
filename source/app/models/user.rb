2e76614210e3d7c01ebf096c1c743aacaa52860a
= <=>
2014-03-21 17:22:17 -0500

    corrected user model. Created show user erb. And a partridge in a pear tree.

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :surveys, foreign_key: "creator_id"
   has_many :taken_surveys, foreign_key: "taker_id"
-  has_many :surveys, through: :taken_surveys
+  has_many :reviewed_surveys, through: :taken_surveys, source: :survey
 end
