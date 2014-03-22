0e92f9182246f79c2337dab44ac5897b1d138e95
Tony Ta <tonyta.tt@gmail.com>
2014-03-21 19:55:59 -0500

    polishes styling for create survey

--- source/public/js/survey_builder.js
+++ source/public/js/survey_builder.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('form').on('click', '.add-question', function(event){
     event.preventDefault();
-    console.log('hello');
     $($(this).siblings('ol')).append("<li><input type='text' name='survey[questions][][question]' placeholder='question'><button class='remove-button'>✖</button><button class='button add-option'>add option</button><ul><li><input type='text' name='survey[questions][][options][]' placeholder='option'><button class='remove-button'>✖</button></li></ul></li>");
   });
   $('form').on('click', '.add-option', function(event){
